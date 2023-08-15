1a21f53c8815d8696144f285c055cddaae5a22b9
bagauov <bagauov@proitr.ru>
2023-08-15 19:50:21 +0300

    create user extension and crud

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/dao/UserDataUserDAO.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/dao/UserDataUserDAO.java
@@ -1,5 +1,6 @@
 package guru.qa.niffler.db.dao;
 
+import guru.qa.niffler.db.model.UserDataEntity;
 import guru.qa.niffler.db.model.UserEntity;
 
 import java.util.UUID;
@@ -9,4 +10,8 @@ public interface UserDataUserDAO {
     int createUserInUserData(UserEntity user);
 
     void deleteUserByIdInUserData(UUID userId);
+
+    UserDataEntity getUserInUserDataByUsername(String username);
+
+    void updateUserInUserData(UserDataEntity user);
 }
