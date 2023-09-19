950bbb3119cc65c93ebe6be905b610917ededa52
zympatec3 <130655904+zympatec3@users.noreply.github.com>
2023-09-19 10:16:10 +0300

    vol 7
    (cherry picked from commit 6271e395d0bf7eabf3182589ba36c10d0437be7a)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
@@ -1,16 +1,11 @@
 package guru.qa.niffler.test;
 
-import com.codeborne.selenide.Selenide;
-import guru.qa.niffler.jupiter.annotation.Category;
-import guru.qa.niffler.jupiter.annotation.User;
+import guru.qa.niffler.jupiter.annotation.ApiLogin;
 import guru.qa.niffler.jupiter.annotation.Spend;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
-import guru.qa.niffler.model.UserJson;
-import guru.qa.niffler.page.WelcomePage;
 import io.qameta.allure.Allure;
 import io.qameta.allure.AllureId;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Isolated;
 
@@ -18,42 +13,31 @@ import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static guru.qa.niffler.jupiter.annotation.User.UserType.WITH_FRIENDS;
+import static com.codeborne.selenide.Selenide.open;
 
 @Isolated
 public class SpendingWebTest extends BaseWebTest {
 
     private static final String user = "dima";
-    private static final String category = "Пивко на яхте";
 
-    @BeforeEach
-    void doLogin(@User(userType = WITH_FRIENDS) UserJson userForTest) {
-        WelcomePage welcomePage = Selenide.open("http://127.0.0.1:3000/main", WelcomePage.class);
-        $("input[name='username']").setValue(userForTest.getUsername());
-        $("input[name='password']").setValue(userForTest.getPassword());
-        $("button[type='submit']").click();
-    }
-
-    @Category(
-            username = user,
-            category = category
-    )
+    @ApiLogin(username = user, password = "12345")
     @Spend(
             username = user,
             description = "Рыбалка на Ладоге",
-            category = category,
+            category = "Рыбалка",
             amount = 14000.00,
             currency = CurrencyValues.RUB
     )
     @Test
     @AllureId("100")
-    void spendingShouldBeDeletedAfterDeleteAction(SpendJson createdSpend,
-                                                  @User(userType = WITH_FRIENDS) UserJson userForTest) {
+    void spendingShouldBeDeletedAfterDeleteAction(SpendJson createdSpend) {
+        open(CFG.nifflerFrontUrl() + "/main");
+
         $(".spendings__content tbody")
                 .$$("tr")
                 .find(text(createdSpend.getDescription()))
-                .scrollTo()
-                .$("td")
+                .$$("td")
+                .first()
                 .scrollTo()
                 .click();
 
